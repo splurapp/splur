117569ac76599867ed96d861c2adf71d21c50074
DNI9 <itsr9boi@gmail.com>
2023-11-25 17:07:41 +0530

    add suggestions title

--- src/routes/setup/AccountSetup.tsx
+++ src/routes/setup/AccountSetup.tsx
@@ -136,7 +136,7 @@ export default function AccountSetup() {
     <main className="p-3 flex flex-col gap-5 h-screen py-6 px-5">
       <section>
         <h1 className="text-4xl font-medium">Let's setup your account!</h1>
-        <p>Account can be your bank, credit card or your wallet, pick one or create new</p>
+        <p>Account can be your bank, credit card or your wallet</p>
       </section>
 
       <section className="flex flex-col gap-2">
@@ -149,23 +149,33 @@ export default function AccountSetup() {
           <EditWalletModal isOpen={isModalOpen} onSave={handleUpdate} wallet={currentWallet} />
         )}
 
-        <div className="flex flex-row flex-wrap gap-3">
-          {defaultWallets?.map(wallet => (
-            <button
-              key={wallet.name}
-              className="btn btn-neutral btn-outline"
-              onClick={() => createWallet(wallet)}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
-                <path
-                  d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"
-                  fill="rgba(255,255,255,1)"
-                ></path>
-              </svg>
-              {wallet.name}
-            </button>
-          ))}
-        </div>
+        {!!defaultWallets?.length && (
+          <>
+            <h1 className="text-xl">Suggestions</h1>
+            <div className="flex flex-row flex-wrap gap-3">
+              {defaultWallets.map(wallet => (
+                <button
+                  key={wallet.name}
+                  className="btn btn-neutral btn-outline"
+                  onClick={() => createWallet(wallet)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="20"
+                    height="20"
+                  >
+                    <path
+                      d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"
+                      fill="rgba(255,255,255,1)"
+                    ></path>
+                  </svg>
+                  {wallet.name}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
       </section>
 
       <section className="mt-auto join join-vertical">
