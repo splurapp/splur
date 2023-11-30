24e41fc07a10266860c5faabf427a6ff0c140031
DNI9 <itsr9boi@gmail.com>
2023-12-01 00:52:35 +0530

    update welcome screen modal

--- src/routes/setup/Hello.tsx
+++ src/routes/setup/Hello.tsx
@@ -46,12 +46,12 @@ export default function Hello() {
         <button className="btn btn-neutral btn-wide mt-5">Let's Go</button>
       </Link>
 
-      <dialog id="pwa-prompt-modal" className="modal" open={isModalOpen}>
+      <dialog id="pwa-prompt-modal" className="modal modal-bottom" open={isModalOpen}>
         <div className="modal-box">
-          <h1 className="text-2xl mb-2">Install as PWA</h1>
+          <h1 className="text-2xl mb-2">Installation</h1>
           <p>
-            This app is intended to be used as PWA, we recommend to install the app to directly
-            launch from home screen, along with many other PWA exclusive feature.
+            This app is intended to be used as standalone app, we recommend installing the app to
+            directly launch from home screen, along with many other exclusive feature.
           </p>
 
           <button
@@ -64,14 +64,14 @@ export default function Hello() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
               <path
                 d="M12.0007 10.5865L16.9504 5.63672L18.3646 7.05093L13.4149 12.0007L18.3646 16.9504L16.9504 18.3646L12.0007 13.4149L7.05093 18.3646L5.63672 16.9504L10.5865 12.0007L5.63672 7.05093L7.05093 5.63672L12.0007 10.5865Z"
-                fill="rgba(255,255,255,1)"
+                fill="currentColor"
               ></path>
             </svg>
           </button>
 
           <div className="modal-action">
             <button className="btn btn-primary w-full" onClick={handlePwaInstall}>
-              Install as PWA
+              Add to homescreen
             </button>
           </div>
         </div>
