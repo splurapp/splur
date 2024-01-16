e99984d815c10776e4e0fc573216fffb710ceed7
dni9 <itsr9boi@gmail.com>
2024-01-17 00:42:35 +0530

    migrate track page

---
+++ src/routes/track/TrackerForm.tsx
@@ -0,0 +1,157 @@
+import type { Category } from "@/model/schema";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
+import * as dayjs from "dayjs";
+import { useMemo } from "react";
+import { useFetcher } from "react-router-dom";
+import type { LoaderData as TrackLoaderData } from "./track.service";
+
+interface Props {
+  exchangeType: string;
+  data: TrackLoaderData;
+}
+
+export default function TrackerForm({ exchangeType, data }: Props) {
+  const fetcher = useFetcher<{ error?: string }>();
+  const timestamp = data.transaction?.timestamp ?? new Date();
+
+  const categories = useMemo<Category[]>(() => {
+    return data.categories.filter(category =>
+      category.types.includes(exchangeType === "Income" ? "Income" : "Expense"),
+    );
+  }, [exchangeType, data.categories]);
+
+  return (
+    <fetcher.Form method="post" className="space-y-2">
+      <input type="hidden" name="exchangeType" value={exchangeType} />
+      <Input
+        type="text"
+        id="title"
+        name="title"
+        defaultValue={data.transaction?.title ?? ""}
+        placeholder="enter title"
+        variant="faded"
+        minLength={2}
+        maxLength={50}
+      />
+
+      <Input
+        isRequired
+        type="datetime-local"
+        id="timestamp"
+        name="timestamp"
+        defaultValue={dayjs(timestamp).format().split("+")[0]}
+        variant="faded"
+      />
+
+      <Input
+        isRequired
+        id="amount"
+        name="amount"
+        type="number"
+        min={0}
+        placeholder="enter amount"
+        defaultValue={data?.transaction?.amount === 0 ? "" : data.transaction?.amount.toString()}
+        variant="faded"
+      />
+
+      {exchangeType !== "Transfer" ? (
+        <>
+          <Select
+            aria-label="category"
+            isRequired
+            name="categoryId"
+            id="categoryId"
+            variant="faded"
+            placeholder="select category"
+            defaultSelectedKeys={
+              data.transaction?.category?.id
+                ? [data.transaction?.category?.id.toString()]
+                : undefined
+            }
+          >
+            {categories.map(category => (
+              <SelectItem
+                key={category.id!}
+                value={category.id}
+                textValue={`${category.icon} ${category.name}`}
+              >
+                {category.icon} {category.name}
+              </SelectItem>
+            ))}
+          </Select>
+
+          <Select
+            aria-label="Wallet selection"
+            isRequired
+            name="walletId"
+            id="walletId"
+            placeholder="select wallet"
+            variant="faded"
+            defaultSelectedKeys={
+              data.transaction?.walletId ? [data.transaction.walletId.toString()] : undefined
+            }
+          >
+            {data.wallets.map(wallet => (
+              <SelectItem key={wallet.id!} value={wallet.id}>
+                {wallet.name}
+              </SelectItem>
+            ))}
+          </Select>
+        </>
+      ) : (
+        <>
+          <Select
+            aria-label="Transfer from wallet"
+            isRequired
+            name="transferFrom"
+            id="transferFrom"
+            placeholder="from wallet"
+            variant="faded"
+            defaultSelectedKeys={
+              data.transaction?.transferFrom
+                ? [data.transaction?.transferFrom.toString()]
+                : undefined
+            }
+          >
+            {data.wallets.map(wallet => (
+              <SelectItem key={wallet.id ?? 0} value={wallet.id}>
+                {wallet.name}
+              </SelectItem>
+            ))}
+          </Select>
+          <Select
+            aria-label="Transfer to wallet"
+            isRequired
+            name="transferTo"
+            id="transferTo"
+            placeholder="to wallet"
+            variant="faded"
+            defaultSelectedKeys={
+              data.transaction?.transferTo ? [data.transaction?.transferTo.toString()] : undefined
+            }
+          >
+            {data.wallets.map(wallet => (
+              <SelectItem key={wallet.id ?? 0} value={wallet.id}>
+                {wallet.name}
+              </SelectItem>
+            ))}
+          </Select>
+        </>
+      )}
+
+      {fetcher.data && "error" in fetcher.data && (
+        <p className="my-2 text-sm text-danger">{fetcher.data.error}</p>
+      )}
+
+      <Button
+        fullWidth
+        size="lg"
+        isDisabled={fetcher.state === "submitting"}
+        color="primary"
+        type="submit"
+      >
+        Submit
+      </Button>
+    </fetcher.Form>
+  );
+}
--- src/routes/track/index.tsx
+++ src/routes/track/index.tsx
@@ -1,222 +1,92 @@
-import type { Category } from "@/model/schema";
 import { ExchangeType } from "@/model/schema";
-import { useMemo, useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Tab,
+  Tabs,
+  useDisclosure,
+} from "@nextui-org/react";
+import { useState } from "react";
 import { useFetcher, useLoaderData } from "react-router-dom";
 import type { z } from "zod";
+import TrackerForm from "./TrackerForm";
 import type { LoaderData } from "./track.service";
 
 export default function Track() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const fetcher = useFetcher<{ error?: string }>();
   const data = useLoaderData() as LoaderData;
   const [exchangeType, setExchangeType] = useState<z.infer<typeof ExchangeType>>(
     data?.transaction?.exchangeType ?? ExchangeType.enum.Expense,
   );
-  const categories = useMemo<Category[]>(() => {
-    return data.categories.filter(category =>
-      category.types.includes(exchangeType === "Income" ? "Income" : "Expense"),
-    );
-  }, [exchangeType, data.categories]);
-
-  const timestamp = data.transaction?.timestamp ?? new Date();
 
   return (
     <main>
-      <h1 className="mb-4 text-2xl font-bold">{!data.transaction ? "Add" : "Edit"} transaction</h1>
-      <div role="tablist" className="tabs-boxed tabs bg-base-200 w-full">
+      <Tabs
+        fullWidth
+        color="primary"
+        size="md"
+        selectedKey={exchangeType}
+        onSelectionChange={key => setExchangeType(ExchangeType.parse(key))}
+      >
         {[ExchangeType.enum.Income, ExchangeType.enum.Expense, ExchangeType.enum.Transfer].map(
           type => (
-            <button
-              disabled={
+            <Tab
+              isDisabled={
                 data.transaction?.exchangeType === "Transfer" &&
                 (type === "Expense" || type === "Income")
               }
-              type="button"
               key={type}
-              role="tab"
-              className={`tab flex-1 ${type === exchangeType ? "tab-active" : ""}`}
-              onClick={() => setExchangeType(type)}
+              title={type}
             >
-              {type}
-            </button>
+              <TrackerForm exchangeType={exchangeType} data={data} />
+            </Tab>
           ),
         )}
-      </div>
-
-      <fetcher.Form method="post">
-        <input type="hidden" name="exchangeType" value={exchangeType} />
-        <div className="form-control mb-2 w-full">
-          <label htmlFor="title" className="label">
-            Title
-          </label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            minLength={2}
-            maxLength={50}
-            className="input input-bordered"
-            placeholder="Enter title"
-            defaultValue={data.transaction?.title ?? ""}
-          />
-        </div>
-
-        <div className="form-control mb-2 w-full">
-          <label htmlFor="timestamp" className="label">
-            Date
-          </label>
-          <input
-            required
-            type="datetime-local"
-            id="timestamp"
-            name="timestamp"
-            className="input input-bordered w-full"
-            placeholder="Select Date"
-            defaultValue={`${timestamp.getFullYear()}-${
-              timestamp.getMonth() + 1
-            }-${timestamp.getDate()}T${timestamp.toTimeString().slice(0, 5)}`}
-          />
-        </div>
-
-        <div className="form-control mb-2 w-full">
-          <label htmlFor="amount" className="label">
-            Amount
-          </label>
-          <input
-            required
-            type="number"
-            id="amount"
-            name="amount"
-            min={0}
-            className="input input-bordered"
-            placeholder="Enter amount"
-            defaultValue={data.transaction?.amount ?? 0}
-          />
-        </div>
-
-        {exchangeType !== "Transfer" ? (
-          <>
-            <div className="form-control mb-2 w-full">
-              <label htmlFor="categoryId" className="label">
-                Category
-              </label>
-              <select
-                required
-                name="categoryId"
-                id="categoryId"
-                className="select select-bordered"
-                defaultValue={data.transaction?.categoryId ?? ""}
-              >
-                <option value="" disabled>
-                  Select category
-                </option>
-                {categories.map(category => (
-                  <option key={category.id} value={category.id}>
-                    {category.icon} {category.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div className="form-control mb-2 w-full">
-              <label htmlFor="walletId" className="label">
-                Wallet
-              </label>
-              <select
-                required
-                name="walletId"
-                id="walletId"
-                className="select select-bordered"
-                defaultValue={data.transaction?.walletId ?? ""}
-              >
-                <option value="" disabled>
-                  Select wallet
-                </option>
-                {data.wallets.map(wallet => (
-                  <option key={wallet.id} value={wallet.id}>
-                    {wallet.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="form-control mb-2 w-full">
-              <label htmlFor="transferFrom" className="label">
-                From
-              </label>
-              <select
-                required
-                name="transferFrom"
-                id="transferFrom"
-                className="select select-bordered"
-                defaultValue={data.transaction?.transferFrom ?? ""}
-              >
-                <option value="" disabled>
-                  Select wallet
-                </option>
-                {data.wallets.map(wallet => (
-                  <option key={wallet.id} value={wallet.id}>
-                    {wallet.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="form-control mb-2 w-full">
-              <label htmlFor="transferTo" className="label">
-                To
-              </label>
-              <select
-                required
-                name="transferTo"
-                id="transferTo"
-                className="select select-bordered"
-                defaultValue={data.transaction?.transferTo ?? ""}
-              >
-                <option value="" disabled>
-                  Select wallet
-                </option>
-                {data.wallets.map(wallet => (
-                  <option key={wallet.id} value={wallet.id}>
-                    {wallet.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </>
-        )}
-
-        {fetcher.data && "error" in fetcher.data && (
-          <p className="text-error my-2 text-sm">{fetcher.data.error}</p>
-        )}
-
-        <button
-          disabled={fetcher.state === "submitting"}
-          type="submit"
-          className="btn btn-primary w-full"
-        >
-          Submit
-        </button>
-      </fetcher.Form>
+      </Tabs>
 
       {data.transaction ? (
-        <fetcher.Form
-          method="delete"
-          onSubmit={e => {
-            if (!confirm("Please confirm you want to delete this transaction.")) {
-              e.preventDefault();
-            }
-          }}
+        <Button
+          color="danger"
+          fullWidth
+          variant="bordered"
+          isDisabled={fetcher.state === "submitting"}
+          onPress={onOpen}
         >
-          <button
-            disabled={fetcher.state === "submitting"}
-            type="submit"
-            className="btn bg-base-200 mt-1 w-full"
-          >
-            Delete
-          </button>
-        </fetcher.Form>
+          Delete
+        </Button>
       ) : null}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} backdrop="blur">
+        <ModalContent>
+          {onClose => (
+            <>
+              <ModalHeader></ModalHeader>
+              <ModalBody>
+                <p>Are you sure you want to delete this ?</p>
+              </ModalBody>
+              <ModalFooter className="flex flex-col gap-2">
+                <Button color="danger" fullWidth variant="flat" onPress={onClose}>
+                  Nah
+                </Button>
+                <Button
+                  fullWidth
+                  color="primary"
+                  onPress={() => {
+                    fetcher.submit(null, { method: "DELETE" });
+                    onClose();
+                  }}
+                >
+                  I'm sure
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </main>
   );
 }
